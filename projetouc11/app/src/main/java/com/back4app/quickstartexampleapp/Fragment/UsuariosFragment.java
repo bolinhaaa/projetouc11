0577f12b11c75d77e4e77435b69cbd83ca345b6f
JONATA DA SILVA BUENO <jonata.sbueno@senacsp.edu.br>
2019-10-01 08:58:46 -0300

    Fragment

--- projetouc11/app/src/main/java/com/back4app/quickstartexampleapp/Fragment/UsuariosFragment.java
+++ projetouc11/app/src/main/java/com/back4app/quickstartexampleapp/Fragment/UsuariosFragment.java
@@ -1,6 +1,35 @@
 package com.back4app.quickstartexampleapp.Fragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.back4app.quickstartexampleapp.R;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
 
 public class UsuariosFragment extends Fragment {
+    private ListView listView;
+    private ArrayAdapter<ParseUser> adapter;
+    private ArrayList<ParseUser> usuarios;
+    private ParseQuery<ParseUser> query;
+
+    public UsuariosFragment() {
+        // Requista de uma construtor publico
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        //inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_usuarios,container,false);
+
+    }
 }
